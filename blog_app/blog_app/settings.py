78be5dd688015693f52a22ec9bdef87d7c60e79c
Oluwapelumi <odedereoluwapelumi13@gmail.com>
2022-04-03 01:23:19 +0200

    whitenoise and static root

--- blog_app/blog_app/settings.py
+++ blog_app/blog_app/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
 
 ROOT_URLCONF = 'blog_app.urls'
@@ -120,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles')
 
 LOGIN_REDIRECT_URL= '/'
 
